6d0c3effaae46d19ebdbf3e0fbb91fd5b2e7c06d
Alexsander <alexdonay@gmail.com>
2022-07-17 17:04:40 -0300

    Ajustes referente ao index.html

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import Game from '../Game/Game';
-import './App.css';
-
 
 function App() {
+
   return (
+    
     <div className="App">
       <header className="App-header">
+      
       <Game />
       </header>
       
